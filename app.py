c3694098e2bcc956272ce599db2535ce50615e90
Connor <connordrandall@gmail.com>
2026-06-11 23:32:45 +1000

    Added comments to python, integrated python with html

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template
-from main import getTemp, getWind, getHum
+from main import getTemp, getWind, getHum, getRain, data
+from datetime import datetime
 
+#Create Flask application instance
 app = Flask(__name__)
 
-getTemp()
-getWind()
-getHum()
 
+#Define the route for the home page
+#When a user visits "/", Flask render the webpage.html template
 @app.route("/")
 def index():
-    return render_template("base.html")
+    #Generate weather graphs when the application starts
+    getTemp()
+    getWind()
+    getHum()
+    getRain()
 
+    #Get last update date and time
+    last_updated = data["observations"]["data"][0]["local_date_time"]
+
+
+    return render_template("webpage.html", last_updated=last_updated)
+
+#Start the Flask developmenmt server
 if __name__ == "__main__":
     app.run()
--- main.py
+++ main.py
@@ -3,25 +3,34 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import matplotlib.dates as mdates
 
+#HTTP header that identifies the application when requesting data from BOM API
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 
+#Request the latest weather observations from BOM
 response = requests.get(f"https://www.bom.gov.au/fwo/IDV60801/IDV60801.94857.json", headers=headers)
 
+#If above request succeeds, convert JSON response into python dictionary
 if response.status_code == 200:
     data = response.json()
 
+#Function to generate Temperature Graph
 def getTemp():
+    #Initialise list variables to store x-axis (Time) and y-axis values (Temperature)
     x = []
     y = []
 
+    #Retrieve weather observations from BOM
     observations = data["observations"]["data"]
 
+    #Determine the timestamp of most recent observation
     latest = datetime.strptime(observations[0]["local_date_time_full"], "%Y%m%d%H%M%S")
 
+    #Calculate timestamp from 12 hours ago
     last_time = latest - timedelta(hours=12)
 
+    #Collect Temperature reading from the last 12 hours
     for observation in observations:
         observation_time = datetime.strptime(observation["local_date_time_full"], "%Y%m%d%H%M%S")
 
@@ -29,36 +38,46 @@ def getTemp():
             x.append(observation_time)
             y.append(observation["air_temp"])
 
+    #Reverse list so that the graphs read in order, from oldest to newest
     x.reverse()
     y.reverse()
 
+    #Create and format the graph itself
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.plot(x, y, marker='o', linewidth=2)
 
+    #Display time labels for every hour of the last 12 hours
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M %p'))
-    ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+    ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
 
+    #Label graph and it axes
     ax.set_title("Temperature - Last 12 Hours")
     ax.set_xlabel("Time")
-    ax.set_ylabel("Temp (Celcius")
+    ax.set_ylabel("Temp (Celcius)")
 
+    #Plot the graph and save the image to display
     plt.xticks(rotation=45)
     plt.grid(True)
     plt.tight_layout()
-
     plt.savefig("static/temp_graph.png")
     plt.close()
 
+#Function to generate Wind Graph
 def getWind():
+    #Initialise list variables to store x-axis (Time) and y-axis values (Wind)
     x = []
     y = []
 
+    #Retrieve weather observations from BOM
     observations = data["observations"]["data"]
 
+    #Determine the timestamp of most recent observation
     latest = datetime.strptime(observations[0]["local_date_time_full"], "%Y%m%d%H%M%S")
 
+    #Calculate timestamp from 12 hours ago
     last_time = latest - timedelta(hours=12)
 
+    #Collect Wind reading from the last 12 hours
     for observation in observations:
         observation_time = datetime.strptime(observation["local_date_time_full"], "%Y%m%d%H%M%S")
 
@@ -68,37 +87,47 @@ def getWind():
             x.append(observation_time)
             y.append(wind_speed)
 
+    #Reverse list so that the graphs read in order, from oldest to newest
     x.reverse()
     y.reverse()
 
+    #Create and format the graph itself
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.plot(x, y, marker='o', linewidth=2)
 
+    #Display time labels for every hour of the last 12 hours
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M %p'))
-    ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+    ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
 
+    #Label graph and it axes
     ax.set_ylim(bottom=0)
     ax.set_title("Wind Speed - Last 12 Hours")
     ax.set_xlabel("Time")
     ax.set_ylabel("Wind (km/h)")
 
+    #Plot the graph and save the image to display
     plt.xticks(rotation=45)
     plt.grid(True)
     plt.tight_layout()
-
     plt.savefig("static/wind_graph.png")
     plt.close()
 
+#Function to generate Humidity Graph
 def getHum():
+    #Initialise list variables to store x-axis (Time) and y-axis values (Humidity)
     x = []
     y = []
 
+    #Retrieve weather observations from BOM
     observations = data["observations"]["data"]
 
+    #Determine the timestamp of most recent observation
     latest = datetime.strptime(observations[0]["local_date_time_full"], "%Y%m%d%H%M%S")
 
+    #Calculate timestamp from 12 hours ago
     last_time = latest - timedelta(hours=12)
 
+    #Collect Humidity reading from the last 12 hours
     for observation in observations:
         observation_time = datetime.strptime(observation["local_date_time_full"], "%Y%m%d%H%M%S")
 
@@ -108,23 +137,77 @@ def getHum():
             x.append(observation_time)
             y.append(humidity)
 
+    #Reverse list so that the graphs read in order, from oldest to newest
     x.reverse()
     y.reverse()
 
+    #Create and format the graph itself
     fig, ax = plt.subplots(figsize=(10, 5))
     ax.plot(x, y, marker='o', linewidth=2)
 
+    #Display time labels for every hour of the last 12 hours
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M %p'))
-    ax.xaxis.set_major_locator(mdates.HourLocator(interval=2))
+    ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
 
+    #Label graph and it axes
     ax.set_ylim(0, 100)
     ax.set_title("Relative Humidity - Last 12 Hours")
     ax.set_xlabel("Time")
     ax.set_ylabel("Humidity (%)")
 
+    #Plot the graph and save the image to display
     plt.xticks(rotation=45)
     plt.grid(True)
     plt.tight_layout()
-
     plt.savefig("static/humidity_graph.png")
+    plt.close()
+
+#Function to generate Rainfall Graph
+def getRain():
+    #Initialise list variables to store x-axis (Time) and y-axis values (Rainfall)
+    x = []
+    y = []
+
+    #Retrieve weather observations from BOM
+    observations = data["observations"]["data"]
+
+    #Determine the timestamp of most recent observation
+    latest = datetime.strptime(observations[0]["local_date_time_full"], "%Y%m%d%H%M%S")
+
+    #Calculate timestamp from 12 hours ago
+    last_time = latest - timedelta(hours=12)
+
+    #Collect Humidity reading from the last 12 hours
+    for observation in observations:
+        observation_time = datetime.strptime(observation["local_date_time_full"], "%Y%m%d%H%M%S")
+
+        rain = float(observation.get("rain_trace", 0))
+
+        if observation_time >= last_time:
+            x.append(observation_time)
+            y.append(rain)
+
+    #Reverse list so that the graphs read in order, from oldest to newest
+    x.reverse()
+    y.reverse()
+
+    #Create and format the graph itself
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.bar(x, y, width=0.05)
+
+    #Display time labels for every hour of the last 12 hours
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M %p'))
+    ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
+
+    #Label graph and it axes
+    ax.set_ylim(bottom=0)
+    ax.set_title("Rainfall - Last 12 Hours")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Rain (mm)")
+
+    #Plot the graph and save the image to display
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("static/rain_graph.png")
     plt.close()
